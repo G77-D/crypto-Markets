c69de323f69b738974341340762462855611cefa
Gary Dineen <dineen77@hotmail.com>
2023-09-26 20:19:19 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect}from 'react'
 import axios from "axios";
+import Coins from "./components/Coins";
 
-function App() {
-	const [ markets, setMarkets] = useState();
 
-	const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en';
 
-	useEffect(() => {
-		axios.get(url) .then((response) => { setMarkets.apply(response.data);
-				console.log(response.data[0]);
-			}).catch((error) => {
+const App = () => {
+
+	const [coins, setCoins] = useState ([])
+	
+	const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en";
+
+useEffect(() => {
+		axios.get(url).then((response) => {
+				setCoins(response.data);
+				console.log(response.data[0])
+			})
+			.catch((error) => {
 				console.log(error);
 			});
-	}, []);
+}, []);
+
+
 
 	return (
-<>
-
-</>
+		<>
+		<Coins coins={coins} />
+		</>
 	);
 }
 
-export default App;
-
-// note to self coin are market//
-// this function is get the api, /
+export default App
